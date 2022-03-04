088a28371dfcfe72335ff8f952bfb06696415b94
Kristian Roland <kristian@rolandtech.org>
2022-03-04 12:06:33 +0100

    Update playercontroller.js

--- controller/playercontroller.js
+++ controller/playercontroller.js
@@ -22,7 +22,7 @@ export const CreatePlayer = (name) => {
         GetHoleScore(hole){
             const score = player.holeScore[hole];
             if(typeof score === 'undefined')
-                return null
+                return ''
             return score;
         },
        
